a7ed5918db74f72a0e9d940e1bb4fe2bf2fffeba
James Gault <jamesgault1488@gmail.com>
2021-11-29 05:52:41 -0500

    spaced out navigation links and added hover feature

--- jdphotography/src/components/NavTabs/NavTabs.js
+++ jdphotography/src/components/NavTabs/NavTabs.js
@@ -2,7 +2,7 @@ import React from "react";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from "react-bootstrap/Navbar";
 // import Container from "react-bootstrap/Container";
-import {useState} from 'react';
+import { useState } from 'react';
 // import Button from 'react-bootstrap/Button';
 import "./styles.css";
 import logo from '../../assets/image/Professional_photo/20210526_1646071.jpg';
@@ -11,29 +11,29 @@ import Offcanvas from 'react-bootstrap/Offcanvas'
 
 function NavTabs() {
 
-        const [show, setShow] = useState(false);
+    const [show, setShow] = useState(false);
 
-        const handleClose = () => setShow(false);
-        const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
 
-        return (<div>
-            <div className="container">
-                <div className="row" id="navTabsRow">
-                    This webpage is under construction. All of the functionality has not been added. Thank You For Your Patience.<br />
+    return (<div>
+        <div className="container">
+            <div className="row" id="navTabsRow">
+                This webpage is under construction. All of the functionality has not been added. Thank You For Your Patience.<br />
 
-                    {/* <Canvas>
+                {/* <Canvas>
                         <ambientLight />
                         <pointLight position={[0, 1, 0]} />
                         <Box position={[-7, 0, 1]} />
                         <Box position={[7, 0, 1]} />
                     </Canvas> */}
-                </div>
             </div>
+        </div>
 
-            {/* <Navbar bg="dark" variant="dark" > */}
-            {/* <Container > */}
-            {/* <Navbar.Brand href="/home">
+        {/* <Navbar bg="dark" variant="dark" > */}
+        {/* <Container > */}
+        {/* <Navbar.Brand href="/home">
                     <img
                         alt="Good Peeps Clothing Line Logo"
                         src={logo}
@@ -55,24 +55,24 @@ function NavTabs() {
                         <Nav.Link href="/"><span id="nav-link-text">Contact</span></Nav.Link>
                     </Nav>
                 </Navbar.Collapse> */}
-            {/* </Container> */}
-            {/* </Navbar> */}
+        {/* </Container> */}
+        {/* </Navbar> */}
 
 
-            <Navbar bg="dark" variant="dark" expand="md" id="nav-data" style={{ position: "fixed", zIndex: 1, width: '100%', top: '0' }}>
-                <Navbar.Brand href="#home"><small><img
-                    alt="Good Peeps Clothing Line Logo"
-                    src={logo}
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                /></small>
-                    Good Peeps
+        <Navbar bg="dark" variant="dark" expand="md" id="nav-data" style={{ position: "fixed", zIndex: 1, width: '100%', top: '0' }}>
+            <Navbar.Brand href="#home"><small><img
+                alt="Good Peeps Clothing Line Logo"
+                src={logo}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+            /></small>
+                Good Peeps
 
-                    <span className="logo"> <p>But don't get it twisted</p></span></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleShow}/>
-                {/* <Navbar.Collapse> */}
-                    {/* <Nav className="justify-content-end" activeKey="/home">
+                <span className="logo"> <p>But don't get it twisted</p></span></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleShow} />
+            {/* <Navbar.Collapse> */}
+            {/* <Nav className="justify-content-end" activeKey="/home">
                         <Nav.Link href="#home"><span id="nav-link-text">Home</span></Nav.Link>
                         <Nav.Link href="#about"><span id="nav-link-text">About</span></Nav.Link>
                         <Nav.Link href="#contact"><span id="nav-link-text">Contact</span></Nav.Link>
@@ -81,40 +81,48 @@ function NavTabs() {
                         <Nav.Link href="#shirts"><span id="nav-link-text">Shirts</span></Nav.Link>
                         <Nav.Link href="#lilpeeps"><span id="nav-link-text">Lil Peeps</span></Nav.Link> */}
 
-                        {/* <Nav.Link href="https://1drv.ms/w/s!AoCauJZ3EHFdgwMB4s6gnDG-WQOe" rel="noreferrer" target="_blank"><span id="nav-link-text">Resume</span></Nav.Link> */}
-                    {/* </Nav> */}
-                {/* </Navbar.Collapse> */}
-            </Navbar>
+            {/* <Nav.Link href="https://1drv.ms/w/s!AoCauJZ3EHFdgwMB4s6gnDG-WQOe" rel="noreferrer" target="_blank"><span id="nav-link-text">Resume</span></Nav.Link> */}
+            {/* </Nav> */}
+            {/* </Navbar.Collapse> */}
+        </Navbar>
 
-            {/* <Button variant="primary" onClick={handleShow}>
+        {/* <Button variant="primary" onClick={handleShow}>
                 Launch
             </Button> */}
 
-            <Offcanvas show={show} onHide={handleClose} >
-                <Offcanvas.Header closeButton style={{ border: 'dodgerblue solid 10px', backgroundColor: 'rgba(25, 25, 25, 1)', color: "white"}}>
-                    <Offcanvas.Title>Nav</Offcanvas.Title>
-                </Offcanvas.Header>
-                {/* color: white;
+        <Offcanvas show={show} onHide={handleClose}>
+            <Offcanvas.Header closeButton style={{backgroundColor: 'rgba(25, 25, 25, 1)', color: "white"}}>
+                <Offcanvas.Title style={{margin: '0 auto'}}>Nav</Offcanvas.Title>
+            </Offcanvas.Header>
+            {/* color: white;
   background-color: rgba(25, 25, 25, 1); */}
-                <Offcanvas.Body style={{display:'grid', gridTemplateColumns: 'repeat(3, 1fr)', backgroundColor: 'rgba(25, 25, 25, 1)', color: 'orange'}}>
-                <Nav className="justify-content-end" activeKey="/home">
+            <Offcanvas.Body style={{ display:'grid', backgroundColor: 'rgba(25, 25, 25, 1)', color: 'orange' }}>
+                <Nav className="justify-content-center" activeKey="/home">
+                    <pre className="pre-links" >
                         <Nav.Link href="#home"><span id="nav-link-text">Home</span></Nav.Link>
+                        <hr/>
                         <Nav.Link href="#about"><span id="nav-link-text">About</span></Nav.Link>
+                        <hr/>
                         <Nav.Link href="#contact"><span id="nav-link-text">Contact</span></Nav.Link>
+                        <hr/>
                         <Nav.Link href="#hoodies"><span id="nav-link-text">Hoodies</span></Nav.Link>
+                        <hr/>
                         <Nav.Link href="#hats"><span id="nav-link-text">Hats</span></Nav.Link>
+                        <hr/>
                         <Nav.Link href="#shirts"><span id="nav-link-text">Shirts</span></Nav.Link>
+                        <hr/>
                         <Nav.Link href="#lilpeeps"><span id="nav-link-text">Lil Peeps</span></Nav.Link>
-                        {/* <Nav.Link href="https://1drv.ms/w/s!AoCauJZ3EHFdgwMB4s6gnDG-WQOe" rel="noreferrer" target="_blank"><span id="nav-link-text">Resume</span></Nav.Link> */}
-                    </Nav>
-                </Offcanvas.Body>
-            </Offcanvas>
+                    </pre>
+                    {/* <Nav.Link href="https://1drv.ms/w/s!AoCauJZ3EHFdgwMB4s6gnDG-WQOe" rel="noreferrer" target="_blank"><span id="nav-link-text">Resume</span></Nav.Link> */}
+                </Nav>
+            </Offcanvas.Body>
+        </Offcanvas>
 
-            {/* <MainSlideShow /> */}
+        {/* <MainSlideShow /> */}
 
 
 
-        </div>)
-    }
+    </div>)
+}
 
-    export default NavTabs;
+export default NavTabs;
