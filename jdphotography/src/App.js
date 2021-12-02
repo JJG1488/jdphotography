c0dfd45c16a5401aed4eb4b9ad61f1677fc3d6ba
James Gault <jamesgault1488@gmail.com>
2021-12-01 21:44:13 -0500

    test

--- jdphotography/src/App.js
+++ jdphotography/src/App.js
@@ -15,7 +15,7 @@ import LilPeeps from './components/pages/LilPeeps/LilPeeps';
 function App() {
   return (
     <div className="App">
-      <Router basename="/jdphotography/">
+      <Router basename="/#">
         <Wrapper>
 
           <NavTabs/>
